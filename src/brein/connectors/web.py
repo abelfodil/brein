ad7b6fe00f3bb53576beb221d85fd4e71f97afb0
Anes Belfodil <ans.belfodil@gmail.com>
2025-03-11 04:55:03 +0000

    Handle PDF and gzipped content

--- src/brein/connectors/web.py
+++ src/brein/connectors/web.py
@@ -1,10 +1,12 @@
 from datetime import datetime
+from io import StringIO
 import urllib.request
 from html2text import HTML2Text
 from brein.models.content import Content
 from brein.models.page import Page, PageType
 from brein.utils.datetime import has_been_less_than, a_month_ago
 from brein.utils.log import log
+import gzip
 
 
 class Web:
@@ -13,38 +15,46 @@ class Web:
     }
 
     html2text = HTML2Text()
+    html2text.ignore_links = True
+    html2text.escape_all = True
 
-    def __init__(self):
-        self.html2text.ignore_links = True
-        self.html2text.escape_all = True
+    def _extract_html(response) -> str:
+        if response.info().get("Content-Encoding") == "gzip":
+            buffer = StringIO(response.read())
+            file = gzip.GzipFile(fileobj=buffer)
+            data = file.read().decode("utf-8")
+            return data
+        else:
+            return response.read().decode("utf-8")
 
-    def _extract_from_page(self, page: Page):
-        if (
-            not page
-            or page.type != PageType.WebPage
-            or (
-                page.content and has_been_less_than(page.content.last_edit, a_month_ago)
-            )
-        ):
+    def _is_pdf(response):
+        return response.getheader("Content-Type") == "application/pdf"
+
+    def scrape_page(page: Page) -> Content:
+        page_name = page.title or page.url
+
+        if page.type != PageType.WebPage:
+            raise Exception(f"Page {page_name} is not a web page.")
+
+        if page.content and has_been_less_than(page.content.last_edit, a_month_ago):
+            log.info(f"Skippping {page_name} since the content is too recent.")
             return None
 
-        page_name = page.title or page.url
         log.info(f'Fetching "{page_name}" web page.')
 
         request = urllib.request.Request(page.url, headers=Web.headers)
         try:
-            raw_html = (
-                urllib.request.urlopen(request, timeout=30).read().decode("utf-8")
-            )
-        except:
-            log.warn(f'Could not fetch "{page_name}" web page.')
-            return None
+            response = urllib.request.urlopen(request, timeout=30)
+            if Web._is_pdf(response):
+                return None
+            raw_html = Web._extract_html(response)
+        except Exception as e:
+            raise Exception(f'Could not fetch "{page_name}" web page. Error: {str(e)}.')
 
         try:
-            text = self.html2text.handle(raw_html)
-        except:
-            log.warn(f'Could not parse "{page_name}" web page.')
-            text = None
+            text = Web.html2text.handle(raw_html)
+        except Exception as e:
+            raise Exception(f'Could not parse "{page_name}" web page. Error: {str(e)}.')
 
         content = Content(page_id=page.id, raw_content=raw_html, text=text)
         if page.content:
@@ -52,8 +62,3 @@ class Web:
             content.id = page.content.id
 
         return content
-
-    def extract(self, pages: list[Page]):
-        contents = (self._extract_from_page(page) for page in pages)
-        contents = (content for content in contents if content)
-        return contents
